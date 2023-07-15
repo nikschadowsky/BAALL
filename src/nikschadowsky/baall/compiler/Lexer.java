5f14814f3fb6c1b17dbf676d0ff82c2bf740db4b
nikschadowsky <n.s-schadowsky@t-online.de>
2023-07-15 18:54:25 +0200

    Lexer Remove Comments + Tests

--- src/nikschadowsky/baall/compiler/Lexer.java
+++ src/nikschadowsky/baall/compiler/Lexer.java
@@ -1,23 +1,56 @@
 package nikschadowsky.baall.compiler;
 
+
 public class Lexer {
 
     private final String path;
 
+    private static final String SINGLE_LINE_COMMENT_REGEX = "(//.*\\R)";
+    private static final String BLOCK_COMMENT_REGEX = "(/\\*(.|\\s)*?\\*/)";
+
     private String content;
 
-    public Lexer(String path){
+    public Lexer(String path) {
         this.path = path;
     }
 
-    public void readContent(){
+    public Lexer readContent() {
         content = FileLoader.loadFileContent(path);
 
-
+        return this;
     }
 
-    private void removeComments(String content){
+    public Lexer removeComments() {
 
+
+        boolean notInString = true;
+
+        if (content.length() > 1) {
+            int i = 1;
+            while (i < content.length()) {
+
+                if (content.charAt(i) == '\"') {
+                    notInString ^= true;
+                } else if (notInString && content.substring(i - 1, i + 1).equals("//")) {
+                    content = content.substring(0, i - 1) + content.substring(i - 1).replaceFirst(SINGLE_LINE_COMMENT_REGEX, "");
+                } else if (notInString && content.substring(i - 1, i + 1).equals("/*")) {
+                    content = content.substring(0, i - 1) + content.substring(i - 1).replaceFirst(BLOCK_COMMENT_REGEX, "");
+                }
+
+                i++;
+            }
+        }
+        return this;
     }
 
+    public Lexer replaceAllWhitespace() {
+
+        content = content.replaceAll("\\s+", " ");
+
+        return this;
+    }
+
+    public String getContent() {
+        return content;
+    }
 }
--- src/nikschadowsky/baall/test/LexerTest.java
+++ src/nikschadowsky/baall/test/LexerTest.java
@@ -1,9 +1,33 @@
 package nikschadowsky.baall.test;
 
+import nikschadowsky.baall.compiler.FileLoader;
+import nikschadowsky.baall.compiler.Lexer;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class LexerTest {
+
+    private Lexer lexer;
+
+    private static final String TEST_PATH = "test/LexerTestFile.txt";
+
     @BeforeEach
     void setUp() {
+
+        lexer = new Lexer(TEST_PATH);
     }
+
+    @Test
+    void testLexerSteps() {
+
+
+        assertEquals(FileLoader.loadFileContent(TEST_PATH), lexer.readContent().getContent());
+
+        assertEquals(FileLoader.loadFileContent("test/RemoveComments.txt"), lexer.removeComments().getContent());
+
+        assertEquals(FileLoader.loadFileContent("test/RemoveWhitespaces.txt"), lexer.replaceAllWhitespace().getContent());
+    }
+
 }

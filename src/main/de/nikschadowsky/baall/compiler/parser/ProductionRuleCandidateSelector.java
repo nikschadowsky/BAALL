e3ffbb6b36878cfd0547555fb8fcd68a9ba48d1f
nikschadowsky <n.s-schadowsky@t-online.de>
2024-01-03 22:06:45 +0100

    feat: 1st iter for determining candidate for production rule

--- src/main/de/nikschadowsky/baall/compiler/parser/ProductionRuleCandidateSelector.java
+++ src/main/de/nikschadowsky/baall/compiler/parser/ProductionRuleCandidateSelector.java
@@ -2,27 +2,64 @@ package de.nikschadowsky.baall.compiler.parser;
 
 import de.nikschadowsky.baall.compiler.grammar.GrammarNonterminal;
 import de.nikschadowsky.baall.compiler.grammar.GrammarProduction;
+import de.nikschadowsky.baall.compiler.grammar.GrammarSymbol;
 import de.nikschadowsky.baall.compiler.lexer.tokens.Token;
 
-import java.util.Iterator;
-import java.util.ListIterator;
-import java.util.Optional;
-import java.util.Set;
+import java.util.*;
 
+/**
+ * File created on 26.12.2023
+ */
 public class ProductionRuleCandidateSelector {
 
-    public ProductionRuleCandidateSelector() {
+    private ProductionRuleCandidateSelector() {
 
     }
 
-    public Optional<GrammarProduction> determineCandidate(GrammarNonterminal symbol, ListIterator<Token> tokenIterator) {
+    public static Optional<GrammarProduction> determineCandidate(GrammarNonterminal symbol, Queue<Token> tokenQueue) {
+
+        tokenQueue = new LinkedList<>(tokenQueue); // defensive copy
+
         ParserStack stack;
 
         Set<GrammarProduction> remainingProductionRules = symbol.getProductionRules();
 
         Iterator<GrammarProduction> remainingProductionRulesIterator = remainingProductionRules.iterator();
 
-        while (tokenIterator.hasNext() && remainingProductionRules.size() > 1) {
+        boolean hasEpsilonRule = remainingProductionRules.stream().anyMatch(GrammarProduction::isEpsilonProduction);
+
+        // Function<Integer, Boolean> f = size -> hasEpsilonRule ? size > 2 : size > 1;
+
+        int index = 0;
+
+        do {
+
+            Token nextToken = tokenQueue.poll();
+
+            while (remainingProductionRulesIterator.hasNext()) {
+
+                GrammarProduction gp = remainingProductionRulesIterator.next();
+
+                if (gp.getSentenceLength() > index) {
+                    // it's sufficient to not check whether the symbol at index is terminal, since the token queue
+                    // always gives a token and nonterminal != token f.a. token.
+
+                    GrammarSymbol current = gp.getSententialForm()[index];
+                    GrammarSymbol candidate;
+
+
+                    if ((current.isTerminal() && !gp.getSententialForm()[index].symbolMatches(nextToken))
+                            || (!current.isTerminal() && determineCandidate((GrammarNonterminal) current, tokenQueue).isEmpty())) {
+                        remainingProductionRulesIterator.remove();
+                    }
+                }
+
+            }
+
+            index++;
+        } while (!tokenQueue.isEmpty() && remainingProductionRules.size() > 1);
+
+        /*while (tokenIterator.hasNext() && remainingProductionRules.size() > 1) {
 
             int index = tokenIterator.nextIndex();
             Token nextToken = tokenIterator.next();
@@ -44,10 +81,10 @@ public class ProductionRuleCandidateSelector {
 
             }
 
-        }
+        }*/
 
 
-        return remainingProductionRules.parallelStream().findAny();
+        return remainingProductionRules.stream().findAny();
     }
 
 

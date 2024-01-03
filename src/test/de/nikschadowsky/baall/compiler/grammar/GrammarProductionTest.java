2309c2a11ba5c3637b96fc32b4dd03ee38473803
nikschadowsky <n.s-schadowsky@t-online.de>
2024-01-03 22:01:59 +0100

    test: GrammarProduction::equals

---
+++ src/test/de/nikschadowsky/baall/compiler/grammar/GrammarProductionTest.java
@@ -0,0 +1,32 @@
+package de.nikschadowsky.baall.compiler.grammar;
+
+import de.nikschadowsky.baall.compiler.lexer.tokens.Token;
+import de.nikschadowsky.baall.compiler.lexer.tokens.TokenType;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+/**
+ * File created on 02.01.2024
+ */
+class GrammarProductionTest {
+
+    @Test
+    void testEquals() {
+
+        GrammarNonterminal lss = new GrammarNonterminal("A");
+
+        GrammarProduction a1 = new GrammarProduction(0, lss, new Token(TokenType.ANY, "A"), lss, new Token(TokenType.ANY, "A"));
+        GrammarProduction a2 = new GrammarProduction(1, lss, new Token(TokenType.ANY, "A"), lss, new Token(TokenType.ANY, "A"));
+
+        GrammarProduction b = new GrammarProduction(2, lss, lss, new Token(TokenType.ANY, "A"));
+        GrammarProduction c = new GrammarProduction(3, lss, new Token(TokenType.ANY, "A"), lss);
+
+
+        assertEquals(a1, a2);
+        assertNotEquals(a1, b);
+        assertNotEquals(a1, c);
+
+    }
+}

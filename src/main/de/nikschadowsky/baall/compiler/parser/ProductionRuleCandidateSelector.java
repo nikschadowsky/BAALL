f68bfb4e88cfd202bd0a86d0704813e59a5f6636
nikschadowsky <n.s-schadowsky@t-online.de>
2024-01-07 15:01:04 +0100

    misc: synchronization

--- src/main/de/nikschadowsky/baall/compiler/parser/ProductionRuleCandidateSelector.java
+++ src/main/de/nikschadowsky/baall/compiler/parser/ProductionRuleCandidateSelector.java
@@ -6,6 +6,7 @@ import de.nikschadowsky.baall.compiler.grammar.GrammarSymbol;
 import de.nikschadowsky.baall.compiler.lexer.tokens.Token;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * File created on 26.12.2023
@@ -16,7 +17,7 @@ public class ProductionRuleCandidateSelector {
 
     }
 
-    public static Optional<GrammarProduction> determineCandidate(GrammarNonterminal symbol, Queue<Token> tokenQueue) {
+    public static Optional<GrammarProduction> determineCandidate1(GrammarNonterminal symbol, Queue<Token> tokenQueue) {
 
         tokenQueue = new LinkedList<>(tokenQueue); // defensive copy
 
@@ -87,5 +88,78 @@ public class ProductionRuleCandidateSelector {
         return remainingProductionRules.stream().findAny();
     }
 
+    public static Optional<GrammarProduction> determineCandidate(GrammarNonterminal lss, Queue<Token> tokenQueue){
+        return
+    }
+
+    private static Optional<ProductionRuleIntermediate> determineIntermediateCandidate(GrammarNonterminal lss, Queue<Token> tokenQueue) {
+
+        tokenQueue = new LinkedList<>(tokenQueue);
+
+        int index = 0;
+
+        Set<GrammarProduction> rulesToCheck = lss.getProductionRules().stream().filter(gp -> !gp.isEpsilonProduction()).collect(Collectors.toSet());
+
+        Optional<GrammarProduction> epsilonRule = lss.getProductionRules().stream().filter(GrammarProduction::isEpsilonProduction).findFirst();
+
+        Set<GrammarNonterminal> deepCheck = new HashSet<>();
+
+
+        // do at least one check.
+        do {
+            Iterator<GrammarProduction> rulesToCheckIterator = rulesToCheck.iterator();
+
+
+
+            while (rulesToCheckIterator.hasNext()) {
+                GrammarProduction rule = rulesToCheckIterator.next();
+
+                if (rule.getSentenceLength() > index) {
+
+                    GrammarSymbol symbol = rule.getSententialForm()[index];
+
+                    if (symbol.isTerminal() && !symbol.symbolMatches(tokenQueue.peek())) {
+                        rulesToCheckIterator.remove();
+                    } else if (!symbol.isTerminal()) {
+                        // check production of this nonterminal
+
+                        deepCheck.add((GrammarNonterminal) symbol);
+                    }
+
+                }
+            }
+
+            for (GrammarNonterminal n : deepCheck) {
+
+            }
+
+            Queue<Token> finalTokenQueue = tokenQueue;
+            Set<GrammarNonterminal> collect = deepCheck.stream()
+                    .map(n -> new ProductionRuleIntermediate(n, determineCandidate(n, finalTokenQueue)))
+                    .filter(ProductionRuleIntermediate::isEmpty).map(ProductionRuleIntermediate::lss)
+                    .collect(Collectors.toSet());
+
+            rulesToCheck.forEach(rule -> {
+                if(!rule.getSententialForm()[index].isTerminal() && collect.contains(rule.getSententialForm()[index])){
+
+                }
+            });
+
+
+        } while (!tokenQueue.isEmpty() && rulesToCheck.size() > 1);
+
+        if (rulesToCheck.isEmpty()) {
+          //  return epsilonRule;
+        }
+
+        //return rulesToCheck.stream().findFirst();
+        return null;
+    }
+
+    private record ProductionRuleIntermediate(GrammarNonterminal lss, Optional<GrammarProduction> candidate) {
+        boolean isEmpty(){
+            return candidate.isEmpty();
+        }
+    }
 
 }

3aba07a37a5c05d1e14e4a013acd6e7c4d862d94
nikschadowsky <n.s-schadowsky@t-online.de>
2024-01-14 19:02:32 +0100

    feat: determineCandidate implemented

--- src/main/de/nikschadowsky/baall/compiler/parser/ProductionRuleCandidateSelector.java
+++ src/main/de/nikschadowsky/baall/compiler/parser/ProductionRuleCandidateSelector.java
@@ -4,8 +4,12 @@ import de.nikschadowsky.baall.compiler.grammar.GrammarNonterminal;
 import de.nikschadowsky.baall.compiler.grammar.GrammarProduction;
 import de.nikschadowsky.baall.compiler.grammar.GrammarSymbol;
 import de.nikschadowsky.baall.compiler.lexer.tokens.Token;
+import org.jetbrains.annotations.NotNull;
 
-import java.util.*;
+import java.util.LinkedList;
+import java.util.Optional;
+import java.util.Queue;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 /**
@@ -14,151 +18,110 @@ import java.util.stream.Collectors;
 public class ProductionRuleCandidateSelector {
 
     private ProductionRuleCandidateSelector() {
-
     }
 
-    public static Optional<GrammarProduction> determineCandidate1(GrammarNonterminal symbol, Queue<Token> tokenQueue) {
-
-        tokenQueue = new LinkedList<>(tokenQueue); // defensive copy
-
-        ParserStack stack;
-
-        Set<GrammarProduction> remainingProductionRules = symbol.getProductionRules();
-
-        Iterator<GrammarProduction> remainingProductionRulesIterator = remainingProductionRules.iterator();
-
-        boolean hasEpsilonRule = remainingProductionRules.stream().anyMatch(GrammarProduction::isEpsilonProduction);
-
-        // Function<Integer, Boolean> f = size -> hasEpsilonRule ? size > 2 : size > 1;
-
-        int index = 0;
-
-        do {
-
-            Token nextToken = tokenQueue.poll();
-
-            while (remainingProductionRulesIterator.hasNext()) {
-
-                GrammarProduction gp = remainingProductionRulesIterator.next();
-
-                if (gp.getSentenceLength() > index) {
-                    // it's sufficient to not check whether the symbol at index is terminal, since the token queue
-                    // always gives a token and nonterminal != token f.a. token.
-
-                    GrammarSymbol current = gp.getSententialForm()[index];
-                    GrammarSymbol candidate;
-
-
-                    if ((current.isTerminal() && !gp.getSententialForm()[index].symbolMatches(nextToken))
-                            || (!current.isTerminal() && determineCandidate((GrammarNonterminal) current, tokenQueue).isEmpty())) {
-                        remainingProductionRulesIterator.remove();
-                    }
-                }
-
-            }
-
-            index++;
-        } while (!tokenQueue.isEmpty() && remainingProductionRules.size() > 1);
-
-        /*while (tokenIterator.hasNext() && remainingProductionRules.size() > 1) {
-
-            int index = tokenIterator.nextIndex();
-            Token nextToken = tokenIterator.next();
-
-            while (remainingProductionRulesIterator.hasNext()) {
-                GrammarProduction rule = remainingProductionRulesIterator.next();
-
-                if (rule.getSentenceLength() > index) {
-
-                    if (rule.getSententialForm()[index].isTerminal())
-                        if (!rule.getSententialForm()[index].symbolMatches(nextToken)) {
-
-                            remainingProductionRules.remove(rule);
-                        } else {
-
-                        }
-
-                }// when the index is larger than the length of a rule's sentential form,
-
-            }
-
-        }*/
-
-
-        return remainingProductionRules.stream().findAny();
-    }
-
-    public static Optional<GrammarProduction> determineCandidate(GrammarNonterminal lss, Queue<Token> tokenQueue){
-        return
-    }
-
-    private static Optional<ProductionRuleIntermediate> determineIntermediateCandidate(GrammarNonterminal lss, Queue<Token> tokenQueue) {
-
+    /**
+     * Determine a production rule candidate for a given nonterminal. Returns an optional of type
+     * {@link GrammarProduction}.
+     * <p>
+     * If there is only one production rule, then this method will validate it with one step before possibly returning
+     * it. If there is an ε-rule (epsilon-rule) and there is another matching rule, this method returns an optional of
+     * the matching rule and ignores the ε-rule. If there are multiple candidates, but they all get rolled out after
+     * evaluating, this method returns the epsilon rule or an empty optional if there is no ε-rule.
+     *
+     * @param lss        the grammar nonterminal of which to determine a candidate
+     * @param tokenQueue the queue of tokens to evaluate the candidate
+     * @return either
+     * <ul>
+     *     <li>empty optional if there is no candidate (not even an ε-rule)</li>
+     *     <li>optional of an production rule containing either an ε-rule or a regular rule
+     *     depending on whichever matches the queue</li>
+     * </ul>
+     */
+    public static Optional<GrammarProduction> determineCandidate(GrammarNonterminal lss, Queue<Token> tokenQueue) {
         tokenQueue = new LinkedList<>(tokenQueue);
 
-        int index = 0;
+        Set<GrammarProduction> rulesUnfiltered = lss.getProductionRules();
 
-        Set<GrammarProduction> rulesToCheck = lss.getProductionRules().stream().filter(gp -> !gp.isEpsilonProduction()).collect(Collectors.toSet());
+        Queue<Token> finalTokenQueue = tokenQueue;
 
-        Optional<GrammarProduction> epsilonRule = lss.getProductionRules().stream().filter(GrammarProduction::isEpsilonProduction).findFirst();
+        Set<PRIntermediate> rulesEpsilonFiltered =
+                rulesUnfiltered
+                        .stream()
+                        .filter(r -> !r.isEpsilonProduction())
+                        .map(r -> new PRIntermediate(r, finalTokenQueue))
+                        .collect(Collectors.toSet());
 
-        Set<GrammarNonterminal> deepCheck = new HashSet<>();
-
-
-        // do at least one check.
         do {
-            Iterator<GrammarProduction> rulesToCheckIterator = rulesToCheck.iterator();
+            rulesEpsilonFiltered.removeIf(intermediate -> !intermediate.step());
+        } while (rulesEpsilonFiltered.size() > 1 && !tokenQueue.isEmpty());
 
 
-
-            while (rulesToCheckIterator.hasNext()) {
-                GrammarProduction rule = rulesToCheckIterator.next();
-
-                if (rule.getSentenceLength() > index) {
-
-                    GrammarSymbol symbol = rule.getSententialForm()[index];
-
-                    if (symbol.isTerminal() && !symbol.symbolMatches(tokenQueue.peek())) {
-                        rulesToCheckIterator.remove();
-                    } else if (!symbol.isTerminal()) {
-                        // check production of this nonterminal
-
-                        deepCheck.add((GrammarNonterminal) symbol);
-                    }
-
-                }
-            }
-
-            for (GrammarNonterminal n : deepCheck) {
-
-            }
-
-            Queue<Token> finalTokenQueue = tokenQueue;
-            Set<GrammarNonterminal> collect = deepCheck.stream()
-                    .map(n -> new ProductionRuleIntermediate(n, determineCandidate(n, finalTokenQueue)))
-                    .filter(ProductionRuleIntermediate::isEmpty).map(ProductionRuleIntermediate::lss)
-                    .collect(Collectors.toSet());
-
-            rulesToCheck.forEach(rule -> {
-                if(!rule.getSententialForm()[index].isTerminal() && collect.contains(rule.getSententialForm()[index])){
-
-                }
-            });
-
-
-        } while (!tokenQueue.isEmpty() && rulesToCheck.size() > 1);
-
-        if (rulesToCheck.isEmpty()) {
-          //  return epsilonRule;
+        // tokenQueue was empty, and we have an ambiguous result -> no candidate
+        if (rulesEpsilonFiltered.size() > 1) {
+            return Optional.empty();
         }
-
-        //return rulesToCheck.stream().findFirst();
-        return null;
+        // get the determined candidate, or if there is none get an optional epsilon rule or none all together.
+        return rulesEpsilonFiltered
+                .stream()
+                .map(PRIntermediate::getRule)
+                .findAny()
+                .or(() -> rulesUnfiltered.stream().filter(GrammarProduction::isEpsilonProduction).findAny());
     }
 
-    private record ProductionRuleIntermediate(GrammarNonterminal lss, Optional<GrammarProduction> candidate) {
-        boolean isEmpty(){
-            return candidate.isEmpty();
+    /**
+     * Class for containing the state of the candidate selector for each production rule
+     */
+    protected static class PRIntermediate {
+
+        private final ParserStack stack;
+
+        private final Queue<Token> tokenQueue;
+
+        private final GrammarProduction rule;
+
+        private PRIntermediate(@NotNull GrammarProduction rule, @NotNull Queue<Token> tokenQueue) {
+            this.rule = rule;
+            this.tokenQueue = new LinkedList<>(tokenQueue); // defensive copy
+            // init stack
+            stack = new ParserStack();
+            stack.pushSeries(rule.getSententialForm());
+        }
+
+        public GrammarProduction getRule() {
+            return rule;
+        }
+
+        /**
+         * Performs a step in reducing the parser stack to check, whether this production is valid.
+         *
+         * @return if the top of the stack was derived successfully
+         */
+        public boolean step() {
+
+            if (!stack.isEmpty() && !tokenQueue.isEmpty()) {
+                GrammarSymbol symbol = stack.pop();
+
+                if (symbol.isTerminal()) {
+                    return symbol.symbolMatches(tokenQueue.poll());
+                } else {
+                    Optional<GrammarProduction> candidate = determineCandidate((GrammarNonterminal) symbol, tokenQueue);
+
+                    if (candidate.isEmpty()) return false;
+
+                    int stackSizeTarget = stack.size();
+                    stack.pushSeries(candidate.get().getSententialForm());
+
+                    // run stack down until only the initial state minus the stack top remains
+                    while (stack.size() > stackSizeTarget) {
+                        if (!step()) return false; // if step fails, then the outer step has to also report a fail
+                    }
+
+                    return true;
+                }
+            }
+
+            return false;
         }
     }
 

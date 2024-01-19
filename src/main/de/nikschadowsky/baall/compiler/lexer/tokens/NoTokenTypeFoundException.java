ac0884927f08c5ed7343fae66e6765d09c7a09eb
nikschadowsky <n.s-schadowsky@t-online.de>
2024-01-19 19:39:02 +0100

    feat: change superclass of exception

--- src/main/de/nikschadowsky/baall/compiler/lexer/tokens/NoTokenTypeFoundException.java
+++ src/main/de/nikschadowsky/baall/compiler/lexer/tokens/NoTokenTypeFoundException.java
@@ -3,7 +3,7 @@ package de.nikschadowsky.baall.compiler.lexer.tokens;
 /**
  * File created on 21.10.2023
  */
-public class NoTokenTypeFoundException extends Exception {
+public class NoTokenTypeFoundException extends RuntimeException {
 
     public NoTokenTypeFoundException(String msg) {
         super(msg);

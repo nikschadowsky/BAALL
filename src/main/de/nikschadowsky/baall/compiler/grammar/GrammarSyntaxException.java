0f4d077af0a1e38e4b39e34b86aef389338ac2aa
nikschadowsky <n.s-schadowsky@t-online.de>
2024-01-25 22:08:53 +0100

    feat: add another constructor

--- src/main/de/nikschadowsky/baall/compiler/grammar/GrammarSyntaxException.java
+++ src/main/de/nikschadowsky/baall/compiler/grammar/GrammarSyntaxException.java
@@ -1,7 +1,13 @@
 package de.nikschadowsky.baall.compiler.grammar;
 
 public class GrammarSyntaxException extends RuntimeException {
-    public GrammarSyntaxException(String msg) {
-        super(msg);
+
+    public GrammarSyntaxException(String additionalInformation, String reason) {
+        super("Invalid syntax! %s%nReason: %s".formatted(additionalInformation, reason));
     }
+
+    public GrammarSyntaxException(String reason) {
+        this("", reason);
+    }
+
 }

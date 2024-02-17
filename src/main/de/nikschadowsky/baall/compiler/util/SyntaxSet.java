41911b0b91c90f27052ddf657aee482132cff238
nikschadowsky <n.s-schadowsky@t-online.de>
2024-02-17 16:03:47 +0100

    fix: separators now include colon as separator

--- src/main/de/nikschadowsky/baall/compiler/util/SyntaxSet.java
+++ src/main/de/nikschadowsky/baall/compiler/util/SyntaxSet.java
@@ -87,6 +87,7 @@ public class SyntaxSet {
         SEPARATORS.add(";");
         SEPARATORS.add(",");
         SEPARATORS.add("::");
+        SEPARATORS.add(":");
         SEPARATORS.add("@");
         SEPARATORS.add("[");
         SEPARATORS.add("]");

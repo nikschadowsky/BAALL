eca386797882eee33fd94ccee1f1b551760f3822
nikschadowsky <n.s-schadowsky@t-online.de>
2024-02-17 16:00:26 +0100

    feat: exception

--- src/main/de/nikschadowsky/baall/compiler/grammar/GrammarSyntaxException.java
+++ src/main/de/nikschadowsky/baall/compiler/grammar/GrammarSyntaxException.java
@@ -3,7 +3,7 @@ package de.nikschadowsky.baall.compiler.grammar;
 public class GrammarSyntaxException extends RuntimeException {
 
     public GrammarSyntaxException(String additionalInformation, String reason) {
-        super("Invalid syntax! %s%nReason: %s".formatted(additionalInformation, reason));
+        super("Invalid grammar syntax! %s%nReason: %s".formatted(additionalInformation, reason));
     }
 
     public GrammarSyntaxException(String reason) {
---
+++ src/main/de/nikschadowsky/baall/compiler/util/exception/AmbiguityException.java
@@ -0,0 +1,12 @@
+package de.nikschadowsky.baall.compiler.util.exception;
+
+/**
+ * File created on 01.02.2024
+ */
+public class AmbiguityException extends RuntimeException {
+
+    public AmbiguityException(String message) {
+        super(message);
+    }
+
+}

9870af4fbf58f979f71dde5436cebdec8477a277
nikschadowsky <n.s-schadowsky@t-online.de>
2024-02-24 09:44:22 +0100

    refactor: class Main renamed to Compiler

--- src/main/de/nikschadowsky/baall/compiler/Compiler.java
+++ src/main/de/nikschadowsky/baall/compiler/Compiler.java
@@ -2,12 +2,25 @@ package de.nikschadowsky.baall.compiler;
 
 import de.nikschadowsky.baall.compiler.lexer.Lexer;
 
+import java.nio.file.Path;
 import java.util.Scanner;
+import java.util.logging.Logger;
 
-public class Main {
+public class Compiler {
+
+    private final Logger logger = Logger.getLogger("Compiler");
 
     public static void main(String[] args) {
         new Lexer(getInputPath(args));
+
+
+    }
+
+    private Compiler(Path source, Path destination){
+        logger.info("Compiling started.");
+
+        //Program program = new Program();
+
     }
 
     private static String getInputPath(String[] args) {

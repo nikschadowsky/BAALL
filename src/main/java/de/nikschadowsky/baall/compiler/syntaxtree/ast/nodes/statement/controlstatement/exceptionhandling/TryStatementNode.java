bc85e94f5fe9850b276519e384469cfb1dff694b
nikschadowsky <n.s-schadowsky@t-online.de>
2024-08-11 22:46:25 +0200

    feat: add node interface for try statement

---
+++ src/main/java/de/nikschadowsky/baall/compiler/syntaxtree/ast/nodes/statement/controlstatement/exceptionhandling/TryStatementNode.java
@@ -0,0 +1,18 @@
+package de.nikschadowsky.baall.compiler.syntaxtree.ast.nodes.statement.controlstatement.exceptionhandling;
+
+import de.nikschadowsky.baall.compiler.syntaxtree.ast.nodes.program.StatementsNode;
+import de.nikschadowsky.baall.compiler.syntaxtree.ast.nodes.statement.StatementNode;
+
+import java.util.Optional;
+
+/**
+ * @since 11.08.2024
+ */
+public interface TryStatementNode extends StatementNode {
+
+    StatementsNode getBody();
+
+    InterceptStatementNode getInterceptBlock();
+
+    Optional<EnsureStatementNode> getEnsureBlock();
+}

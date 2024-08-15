18a28917fd526f5c47db6b21decb294f333924e6
nikschadowsky <n.s-schadowsky@t-online.de>
2024-08-15 23:04:39 +0200

    feat: diagnostic collector for diagnostics that are created by ast nodes

---
+++ src/main/java/de/nikschadowsky/baall/compiler/syntaxtree/util/NodeDiagnostic.java
@@ -0,0 +1,20 @@
+package de.nikschadowsky.baall.compiler.syntaxtree.util;
+
+import de.nikschadowsky.baall.compiler.symbol.Token;
+
+/**
+ * File created on 14.04.2024
+ */
+public record NodeDiagnostic(String message, Token source, ReportingLevel reportingLevel) {
+
+    @Override
+    public String toString() {
+        String sourceString = source != null ? "@" + source : "";
+
+        return "NodeDiagnostic@%s: %s".formatted(sourceString, message);
+    }
+
+    public enum ReportingLevel {
+        INFO, WARN, ERROR
+    }
+}
---
+++ src/main/java/de/nikschadowsky/baall/compiler/syntaxtree/util/NodeDiagnosticCollector.java
@@ -0,0 +1,28 @@
+package de.nikschadowsky.baall.compiler.syntaxtree.util;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * File created on 14.04.2024
+ */
+public class NodeDiagnosticCollector {
+
+    private final List<NodeDiagnostic> diagnostics = new ArrayList<NodeDiagnostic>();
+
+    public NodeDiagnosticCollector() {
+    }
+
+    public void report(NodeDiagnostic diagnostic) {
+        diagnostics.add(diagnostic);
+    }
+
+    public String createReport() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public List<NodeDiagnostic> getDiagnostics() {
+        return Collections.unmodifiableList(diagnostics);
+    }
+}

8dc66a87e422fbfdc83a5e1f9d02c4e90a9be000
nikschadowsky <n.s-schadowsky@t-online.de>
2024-08-20 22:26:39 +0200

    feat: only allow certain ParseResultAssertions depending on result state

--- src/test/java/de/nikschadowsky/baall/compiler/_utility/ParseResultAssertion.java
+++ src/test/java/de/nikschadowsky/baall/compiler/_utility/ParseResultAssertion.java
@@ -3,7 +3,6 @@ package de.nikschadowsky.baall.compiler._utility;
 
 import de.nikschadowsky.baall.compiler.syntax.analysis.result.ParseResult;
 
-import java.util.NoSuchElementException;
 import java.util.function.Predicate;
 
 /**
@@ -15,36 +14,52 @@ public class ParseResultAssertion<T> extends BaseAssertion<ParseResultAssertion<
         super(actual, ParseResultAssertion.class);
     }
 
-    public ParseResultAssertion<T> isSuccessful() {
-        return truthinessAssert(pr -> "ParseResult is not successful!\n syntax diagnostic: " + pr.getDiagnostic().getMessage(), ParseResult::isSuccessful);
+    public SuccessfulParseResultAssertion<T> isSuccessful() {
+        truthinessAssert(
+                pr -> "ParseResult is not successful!\n syntax diagnostic: " + pr.getDiagnostic()
+                                                                                 .getMessage(),
+                ParseResult::isSuccessful
+        );
+        return new SuccessfulParseResultAssertion<>(actual);
     }
 
-    public ParseResultAssertion<T> isUnsuccessful() {
-        return truthinessAssert(pr -> "ParseResult is successful!", ParseResult::isUnsuccessful);
+    public UnsuccessfulParseResultAssertion<T> isUnsuccessful() {
+        truthinessAssert(pr -> "ParseResult is successful!", ParseResult::isUnsuccessful);
+        return new UnsuccessfulParseResultAssertion<>(actual);
     }
 
-    public ParseResultAssertion<T> resultMatches(Predicate<T> predicate) {
-        return truthinessAssert(pr -> "Result does not match predicate!", pr -> {
-            try {
-                return predicate.test(pr.getParseResult());
-            } catch (NoSuchElementException e) {
-                failWithMessage("No ParseResult could be extracted since ParseResult is unsuccessful!");
-                return false;
-            }
-        });
+    public static class SuccessfulParseResultAssertion<T> extends BaseAssertion<SuccessfulParseResultAssertion<T>, ParseResult<T>> {
+        public SuccessfulParseResultAssertion(ParseResult<T> actual) {
+            super(actual, SuccessfulParseResultAssertion.class);
+        }
+
+        public SuccessfulParseResultAssertion<T> resultMatches(Predicate<T> predicate) {
+            return truthinessAssert(
+                    pr -> "Result does not match predicate!",
+                    pr -> predicate.test(pr.getParseResult())
+            );
+        }
     }
 
-    public ParseResultAssertion<T> syntaxDiagnosticContains(String expected) {
-        return truthinessAssert(pr -> "Syntax diagnostic:\n<%s>\n does not contain:\n<%s>".formatted(
-                pr.getDiagnostic().getMessage(),
-                expected
-        ), pr -> {
-            try {
-                return pr.getDiagnostic().getMessage().contains(expected);
-            } catch (NoSuchElementException e) {
-                failWithMessage("No diagnostic could be extracted since ParseResult is successful!");
-                return false;
-            }
-        });
+    public static class UnsuccessfulParseResultAssertion<T> extends BaseAssertion<UnsuccessfulParseResultAssertion<T>, ParseResult<T>> {
+        public UnsuccessfulParseResultAssertion(ParseResult<T> actual) {
+            super(actual, UnsuccessfulParseResultAssertion.class);
+        }
+
+        public UnsuccessfulParseResultAssertion<T> syntaxDiagnosticContains(String expected) {
+            return truthinessAssert(pr -> "Syntax diagnostic:\n<%s>\n does not contain:\n<%s>".formatted(
+                    pr.getDiagnostic().getMessage(),
+                    expected
+            ), pr -> pr.getDiagnostic().getMessage().contains(expected));
+        }
+
+        public UnsuccessfulParseResultAssertion<T> syntaxDiagnosticEquals(String expected) {
+            return truthinessAssert(
+                    pr -> "Syntax diagnostic:\n<%s>\n does not equal:\n<%s>".formatted(pr.getDiagnostic()
+                                                                                         .getMessage(), expected),
+                    pr -> expected.equals(pr.getDiagnostic().getMessage())
+            );
+        }
+
     }
 }

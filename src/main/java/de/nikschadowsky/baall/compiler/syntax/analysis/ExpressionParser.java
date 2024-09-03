c9a06458b941bc5c16382b591002793671acac17
nikschadowsky <n.s-schadowsky@t-online.de>
2024-09-03 19:30:59 +0200

    docs: add javadoc specification for each parse method

--- src/main/java/de/nikschadowsky/baall/compiler/syntax/analysis/ExpressionParser.java
+++ src/main/java/de/nikschadowsky/baall/compiler/syntax/analysis/ExpressionParser.java
@@ -1,7 +1,6 @@
 package de.nikschadowsky.baall.compiler.syntax.analysis;
 
 
-import de.nikschadowsky.baall.compiler.symbol.Token;
 import de.nikschadowsky.baall.compiler.symbol.TokenQueue;
 import de.nikschadowsky.baall.compiler.syntax.analysis.result.ParseResult;
 import de.nikschadowsky.baall.compiler.syntax.analysis.result.PartialParseResult;
@@ -16,15 +15,41 @@ import de.nikschadowsky.baall.compiler.syntaxtree.ast.nodes.value.ValueNode;
  * @since 25.08.2024
  */
 public interface ExpressionParser {
+
+    /**
+     * Parses an expression. An expression can either be a binary expression, a value, a function call or a unary
+     * operation.
+     *
+     * @param queue queue of the tokens
+     * @return complete parse result of the parsed expression
+     */
     @CompleteParse
     ParseResult<ExpressionNode> parseExpression(TokenQueue queue);
 
+    /**
+     * Parses a value. A value can be either a literal, an identifier access, a function call or a unary expression.
+     *
+     * @param queue queue of the tokens
+     * @return complete parse result of the parsed value
+     */
     @CompleteParse
     ParseResult<ValueNode> parseValue(TokenQueue queue);
 
+    /**
+     * Parses a function call. This parse result can be partial.
+     *
+     * @param queue queue of the tokens
+     * @return partial parse result of the parsed function call.
+     */
     @PartialParse
     PartialParseResult<FunctionCallNode> parseFunctionCall(TokenQueue queue);
 
+    /**
+     * Parses a unary expression in the format of '++IDENTIFIER' or 'IDENTIFIER++'.
+     *
+     * @param queue queue of the tokens
+     * @return complete parse result of the parsed unary expression
+     */
     @CompleteParse
     ParseResult<UnaryExpressionNode> parseUnaryExpression(TokenQueue queue);
 

905e94783abf7c0d4b9a41c42bdea491cfe46ac7
nikschadowsky <n.s-schadowsky@t-online.de>
2025-01-12 21:31:55 +0100

    fix: throws exception to indicate fatal compile error

--- src/main/java/de/nikschadowsky/baall/compiler/files/FileLineReaderStep.java
+++ src/main/java/de/nikschadowsky/baall/compiler/files/FileLineReaderStep.java
@@ -4,9 +4,11 @@ package de.nikschadowsky.baall.compiler.files;
 import de.nikschadowsky.baall.compiler.CompileInformation;
 import de.nikschadowsky.baall.compiler.Step;
 import de.nikschadowsky.baall.compiler.StepOptions;
+import de.nikschadowsky.baall.compiler.output.error.CompileException;
 import de.nikschadowsky.baall.compiler.util.FileLoader;
 import org.jetbrains.annotations.Nullable;
 
+import java.io.IOException;
 import java.nio.file.Path;
 import java.util.List;
 
@@ -20,11 +22,14 @@ public class FileLineReaderStep extends Step<Path, List<String>> {
     }
 
     @Override
-    public List<String> executeStep(Path path, CompileInformation compileInformation) {
-        String content = FileLoader.getFileContent(path);
+    public List<String> executeStep(Path path, CompileInformation compileInformation) throws CompileException {
+        try {
+            String content = FileLoader.getFileContent(path);
+            compileInformation.addInformation("Source Code", content);
 
-        compileInformation.add("Source Code", content);
-
-        return content.lines().toList();
+            return content.lines().toList();
+        }catch (IOException e) {;
+            throw new CompileException("IO exception occurred in step: FILE_LINE_READER", e);
+        }
     }
 }

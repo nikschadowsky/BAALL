bd7a42d24538511636792973e2d1ef5e2ed65647
nikschadowsky <n.s-schadowsky@t-online.de>
2025-01-12 21:26:42 +0100

    feat: add another compilation information method

--- src/main/java/de/nikschadowsky/baall/compiler/CompileInformation.java
+++ src/main/java/de/nikschadowsky/baall/compiler/CompileInformation.java
@@ -4,10 +4,26 @@ package de.nikschadowsky.baall.compiler;
 import org.jetbrains.annotations.NotNull;
 
 /**
+ * Interface for collecting information during the compilation process. Fatal errors can be raised by throwing a
+ * subclass of {@link de.nikschadowsky.baall.compiler.output.error.CompileException}.
+ *
  * @since 11.01.2025
  */
 public interface CompileInformation {
 
-    void add(@NotNull String key, @NotNull Object value);
+    /**
+     * Adds information in form of a key value pair to this compilation process.
+     *
+     * @param key key of the information
+     * @param value information body
+     */
+    void addInformation(@NotNull String key, @NotNull Object value);
+
+    /**
+     * Adds a warning message to this compilation process.
+     *
+     * @param message message
+     */
+    void addWarning(@NotNull String message);
 
 }

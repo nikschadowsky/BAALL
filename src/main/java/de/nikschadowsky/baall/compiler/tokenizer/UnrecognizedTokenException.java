a261ad55341067da06d79338790fe482c25db634
nikschadowsky <n.s-schadowsky@t-online.de>
2025-01-12 22:12:28 +0100

    feat: made exception a compile exception

--- src/main/java/de/nikschadowsky/baall/compiler/tokenizer/UnrecognizedTokenException.java
+++ src/main/java/de/nikschadowsky/baall/compiler/tokenizer/UnrecognizedTokenException.java
@@ -1,8 +1,16 @@
 package de.nikschadowsky.baall.compiler.tokenizer;
 
-public class UnrecognizedTokenException extends RuntimeException {
+import de.nikschadowsky.baall.compiler.lexer.LexerRow;
+import de.nikschadowsky.baall.compiler.output.error.CompileException;
 
+public class UnrecognizedTokenException extends CompileException {
+
+    public UnrecognizedTokenException(LexerRow row, int index) {
+        super("Unrecognized token at index " + index + ": " + row.rowIndex());
+    }
+
+    @Deprecated
     public UnrecognizedTokenException(int index) {
-        super("A Token at Index " + index + " was not recognized as a valid Token!");
+        super("Unrecognized token at index " + index);
     }
 }

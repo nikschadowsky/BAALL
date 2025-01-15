68a9b7654ae966e81ff360d76bb485dc7fa8addc
nikschadowsky <n.s-schadowsky@t-online.de>
2025-01-15 20:54:33 +0100

    fix: remove unnecessary fields

--- src/main/java/de/nikschadowsky/baall/compiler/syntax/error/SyntaxDiagnostic.java
+++ src/main/java/de/nikschadowsky/baall/compiler/syntax/error/SyntaxDiagnostic.java
@@ -1,42 +1,20 @@
 package de.nikschadowsky.baall.compiler.syntax.error;
 
+import de.nikschadowsky.baall.compiler.output.error.Diagnostic;
 import de.nikschadowsky.baall.compiler.symbol.Token;
 
 /**
  * @since 07.04.2024
  */
-public class SyntaxDiagnostic {
+public class SyntaxDiagnostic extends Diagnostic {
 
-    private final int index;
     private final String message;
     private final Token token;
 
-    public SyntaxDiagnostic(String message) {
-        this(null, -1, message);
-    }
-
-    public SyntaxDiagnostic(int index, String message) {
-        this(null, index, message);
-    }
 
     public SyntaxDiagnostic(Token token, String message) {
-        this(token, -1, message);
-    }
-
-    /**
-     * Constructor to initialize all fields.
-     */
-    private SyntaxDiagnostic(Token token, int index, String message) {
-        this.index = index;
         this.message = message;
         this.token = token;
-    }
-
-    /**
-     * @return index, or {@code -1} if no index was set.
-     */
-    public int getIndex() {
-        return index;
     }
 
     public String getMessage() {
@@ -45,5 +23,10 @@ public class SyntaxDiagnostic {
 
     public Token getToken() {
         return token;
+    }
+
+    @Override
+    public String getFormatted() {
+        return "%s:";
     }
 }

609a691e796ed4c5c38a3060acc592388f75470b
nikschadowsky <n.s-schadowsky@t-online.de>
2025-01-23 23:01:16 +0100

    feat: add visitable interface and make AbstractNode inherit it

---
+++ src/main/java/de/nikschadowsky/baall/compiler/syntax/tree/ast/nodes/ASTVisitable.java
@@ -0,0 +1,13 @@
+package de.nikschadowsky.baall.compiler.syntax.tree.ast.nodes;
+
+
+import de.nikschadowsky.baall.compiler.semantic.traversal.ASTVisitor;
+
+/**
+ * @since 23.01.2025
+ */
+public interface ASTVisitable {
+
+    void accept(ASTVisitor visitor);
+    
+}

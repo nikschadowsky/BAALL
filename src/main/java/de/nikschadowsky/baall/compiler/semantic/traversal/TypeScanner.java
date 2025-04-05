27b055dcb3d6050513ef9d215d46ba23bce840df
nikschadowsky <n.s-schadowsky@t-online.de>
2025-04-05 09:07:46 +0200

    feat: transform type scanner to use type table instead of symbol table

--- src/main/java/de/nikschadowsky/baall/compiler/semantic/traversal/TypeScanner.java
+++ src/main/java/de/nikschadowsky/baall/compiler/semantic/traversal/TypeScanner.java
@@ -3,21 +3,22 @@ package de.nikschadowsky.baall.compiler.semantic.traversal;
 import de.nikschadowsky.baall.compiler.output.error.DiagnosticCollector;
 import de.nikschadowsky.baall.compiler.semantic.SemanticDiagnostic;
 import de.nikschadowsky.baall.compiler.semantic.attribute.Scope;
-import de.nikschadowsky.baall.compiler.symbol.SymbolAlreadyExistsException;
-import de.nikschadowsky.baall.compiler.symbol.SymbolTable;
+import de.nikschadowsky.baall.compiler.semantic.type.TypeReference;
+import de.nikschadowsky.baall.compiler.semantic.type.TypeTable;
 import de.nikschadowsky.baall.compiler.symbol.Token;
+import de.nikschadowsky.baall.compiler.symbol.TypeAlreadyExistsException;
 import de.nikschadowsky.baall.compiler.syntax.tree.ast.nodes.statement.assignment.ConstantDeclarationNode;
 import de.nikschadowsky.baall.compiler.syntax.tree.ast.nodes.statement.assignment.VariableDeclarationNode;
 import de.nikschadowsky.baall.compiler.syntax.tree.ast.nodes.typing.PrimitiveTypeNode;
 
 public class TypeScanner extends ScopeTraverser {
 
-    private final SymbolTable table;
+    private final TypeTable table;
     private final DiagnosticCollector<SemanticDiagnostic> diagnosticCollector;
     private static final String ILLEGAL_VAR_TYPE =
             "Cannot use %1%s when declaring a variable! %1$ss can only be declared as constants.";
 
-    public TypeScanner(SymbolTable table, DiagnosticCollector<SemanticDiagnostic> diagnosticCollector) {
+    public TypeScanner(TypeTable table, DiagnosticCollector<SemanticDiagnostic> diagnosticCollector) {
         this.table = table;
         this.diagnosticCollector = diagnosticCollector;
     }
@@ -25,16 +26,16 @@ public class TypeScanner extends ScopeTraverser {
     @Override
     public Boolean visitConstantDeclaration(ConstantDeclarationNode that, Scope scope) {
         if (that.getType() instanceof PrimitiveTypeNode type) {
-            if (type.getKind() == PrimitiveTypeNode.Kind.STRUCT ||
-                    type.getKind() == PrimitiveTypeNode.Kind.EXCEPTION) {
-                Token identifier = that.getIdentifier().getIdentifier();
-                try {
-                    table.registerSymbol(identifier.value(), scope, identifier.lineInformation());
-                } catch (SymbolAlreadyExistsException e) {
-                    diagnosticCollector.addError(new SemanticDiagnostic(
-                            "Type with name '%s' already declared in this scope!".formatted(identifier))
-                    );
+            Token identifier = that.getIdentifier().getIdentifier();
+            try {
+                switch (type.getKind()) {
+                    case STRUCT -> table.registerType(identifier.value(), scope, TypeReference.Kind.STRUCT);
+                    case EXCEPTION -> table.registerType(identifier.value(), scope, TypeReference.Kind.EXCEPTION);
                 }
+            } catch (TypeAlreadyExistsException e) {
+                diagnosticCollector.addError(new SemanticDiagnostic(
+                        "Type with name '%s' already declared in this scope!".formatted(identifier))
+                );
             }
         }
 

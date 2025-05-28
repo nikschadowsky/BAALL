6d69b5758ca3cb03733a812722ede00cd68acefd
nikschadowsky <n.s-schadowsky@t-online.de>
2025-05-28 22:21:39 +0200

    feat: add method for creating baall type for a given type node

--- src/main/java/de/nikschadowsky/baall/compiler/semantic/type/BaallTypeFactory.java
+++ src/main/java/de/nikschadowsky/baall/compiler/semantic/type/BaallTypeFactory.java
@@ -1,20 +1,37 @@
 package de.nikschadowsky.baall.compiler.semantic.type;
 
 
-import de.nikschadowsky.baall.compiler.syntax.tree.ast.nodes.typing.PrimitiveTypeNode;
+import de.nikschadowsky.baall.compiler.symbol.UnresolvedSymbolException;
+import de.nikschadowsky.baall.compiler.syntax.tree.ast.nodes.typing.*;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * @since 27.03.2025
  */
 public class BaallTypeFactory {
 
-    public static BaallTypeFactory create() {
-        return new BaallTypeFactory();
+    private final TypeTable typeTable;
+
+    public BaallTypeFactory(TypeTable typeTable) {
+        this.typeTable = typeTable;
     }
 
-    private BaallTypeFactory() {
+    public BaallType getTypeForNode(TypeNode node) {
+        return switch (node) {
+            case PrimitiveTypeNode p -> createPrimitiveType(p.getKind());
+            case ListTypeNode l -> createListType(getTypeForNode(l.getInnerType()));
+            case FunctionTypeNode f -> createFunctionType(
+                    getTypeForNode(f.getInnerType()),
+                    f.getParameterTypes()
+                     .stream()
+                     .map(this::getTypeForNode)
+                     .toList()
+            );
+            // todo where do i get this scope from? argument?
+            case IdentifierTypeNode i -> createUserDefinedType(typeTable.resolveType(i, null), i.isNoneSafe());
+        };
     }
 
     public PrimitiveType createPrimitiveType(PrimitiveTypeNode.Kind kind) {
@@ -27,8 +44,8 @@ public class BaallTypeFactory {
         };
     }
 
-    public UserDefinedType createUserDefinedType(TypeReference typeReference) {
-        return new UserDefinedTypeImpl(typeReference);
+    public UserDefinedType createUserDefinedType(TypeReference typeReference, boolean isNoneSafe) {
+        return new UserDefinedTypeImpl(typeReference, isNoneSafe);
     }
 
     public FunctionType createFunctionType(BaallType returnType, List<BaallType> parameterTypes) {
@@ -37,6 +54,10 @@ public class BaallTypeFactory {
 
     public ListType createListType(BaallType elementType) {
         return new ListTypeImpl(elementType);
+    }
+
+    public TypeTable getTypeTable() {
+        return typeTable;
     }
 
 }

dcd0c071600f531487ddd8555249bc4f6c72b321
nikschadowsky <n.s-schadowsky@t-online.de>
2025-05-28 21:55:03 +0200

    feat: user types can be unknown and interface now provides access to type reference

--- src/main/java/de/nikschadowsky/baall/compiler/semantic/type/UserDefinedType.java
+++ src/main/java/de/nikschadowsky/baall/compiler/semantic/type/UserDefinedType.java
@@ -1,4 +1,11 @@
 package de.nikschadowsky.baall.compiler.semantic.type;
 
 public sealed interface UserDefinedType extends SimpleType permits UserDefinedTypeImpl {
+
+    TypeReference reference();
+
+    boolean isKnown();
+
+    boolean isUnknown();
+
 }
--- src/main/java/de/nikschadowsky/baall/compiler/semantic/type/UserDefinedTypeImpl.java
+++ src/main/java/de/nikschadowsky/baall/compiler/semantic/type/UserDefinedTypeImpl.java
@@ -1,4 +1,14 @@
 package de.nikschadowsky.baall.compiler.semantic.type;
 
-record UserDefinedTypeImpl(TypeReference reference) implements UserDefinedType {
+record UserDefinedTypeImpl(TypeReference reference, boolean isNoneSafe) implements UserDefinedType {
+
+    @Override
+    public boolean isKnown() {
+        return !isUnknown();
+    }
+
+    @Override
+    public boolean isUnknown() {
+        return reference.getKind() == TypeReference.Kind.UNKNOWN;
+    }
 }
